3406e70eb17da23a79d9edeafcfc94d30cff534d
maria tektova <tkmarie@yandex.ru>
2023-06-21 12:28:54 +0300

    wedwed

--- backend/utils/constants.js
+++
@@ -1,4 +0,0 @@
-const urlRegEx = /^((ftp|http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-/])*)?/;
-module.exports = {
-  urlRegEx,
-};
