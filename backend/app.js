f36a16d8f04da5483f5b6cffdcb6d1a113348b28
maria tektova <tkmarie@yandex.ru>
2023-07-24 13:54:05 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -12,13 +12,14 @@ const NotFound = require('./errors/notFound');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-
+const cors = require('cors');
 connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(cookieParser());
+app.use(cors);
 
 app.use(requestLogger);
 
