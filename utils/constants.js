8fbcff17ee111f591ab71fd9b45cbee134939940
maria tektova <tkmarie@yandex.ru>
2023-08-04 17:09:49 +0300

    cors

--- utils/constants.js
+++ utils/constants.js
@@ -2,3 +2,14 @@ const urlRegEx = /^((ftp|http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Z
 module.exports = {
   urlRegEx,
 };
+
+const allowedCors = [
+  'https://api.mariatektova.diploma.nomoredomains.rocks',
+  'https://api.mariatektova.diploma.nomoredomains.rocks',
+  'http://api.mariatektova.diploma.nomoredomains.rocks',
+  'http://api.mariatektova.diploma.nomoredomains.rocks',
+  'https://localhost:3000',
+  'http://localhost:3000',
+];
+
+module.exports = allowedCors;
