82b627873c1a513dae5a522c02d6413a5f0b739a
maria tektova <tkmarie@yandex.ru>
2023-08-08 12:08:35 +0300

    add cors

--- app.js
+++ app.js
@@ -12,7 +12,27 @@ const NotFound = require('./errors/notFound');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-const cors = require('./middlewares/cors');
+
+const urlList = ['http://mariatektova.diploma.nomoredomains.rocks/', 'https:///mariatektova.diploma.nomoredomains.rocks/'];
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  if (urlList.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  res.header('Access-Control-Allow-Origin', '*');
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  return next();
+});
+
 connectDB();
 
 app.use(express.json());
